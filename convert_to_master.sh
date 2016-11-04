4e62531b64e3b9f900dfffad997de8e1a8c29a17
boardstretcher <szornes@outlook.com>
2016-11-04 15:23:18 -0400

    Update convert_to_master.sh

--- convert_to_master.sh
+++ convert_to_master.sh
@@ -1,7 +1,10 @@
 # convert existing standalone to master
 
+echo "New password for the redis master? "
+read $redis_master_password
+
 sed -i 's/appendonly no/appendonly yes/g' /etc/redis.conf
 sed -i 's/tcp-keepalive 300/tcp-keepalive 60/g' /etc/redis.conf
 sed -i 's/bind 127.0.0.1/#bind 127.0.0.1/g' /etc/redis.conf
-echo "requirepass some_crazy_password" >> /etc/redis.conf
+echo "requirepass $redis_master_password" >> /etc/redis.conf
 systemctl restart redis
