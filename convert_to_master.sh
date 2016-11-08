0744dbcbbd8d369b22361bc824322b514263d8e3
boardstretcher <szornes@outlook.com>
2016-11-08 10:34:00 -0500

    Update convert_to_master.sh

--- convert_to_master.sh
+++ convert_to_master.sh
@@ -7,4 +7,7 @@ sed -i 's/appendonly no/appendonly yes/g' /etc/redis.conf
 sed -i 's/tcp-keepalive 300/tcp-keepalive 60/g' /etc/redis.conf
 sed -i 's/bind 127.0.0.1/#bind 127.0.0.1/g' /etc/redis.conf
 echo "requirepass $redis_master_password" >> /etc/redis.conf
-systemctl restart redis
+
+systemctl stop redis
+systemctl start redis
+sleep 5; systemctl status redis
