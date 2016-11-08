8093943bde4ef8b5a536142c7f66a1f02873a8c1
boardstretcher <szornes@outlook.com>
2016-11-08 10:54:57 -0500

    Update convert_to_slave.sh

--- convert_to_slave.sh
+++ convert_to_slave.sh
@@ -1,14 +1,16 @@
 # convert standalone to master
 
 echo "What is the IP of the redis master?"
-read $redis_master_ip
+read redis_master_ip
 echo "What is the current password for the redis master? "
-read $redis_master_password
+read redis_master_password
 echo "New password for the redis slave?"
-read $redis_slave_password
+read redis_slave_password
 
 sed -i 's/bind 127.0.0.1/#bind 127.0.0.1/g' /etc/redis.conf
 echo "requirepass $redis_slave_password" >> /etc/redis.conf
 echo "slaveof $redis_master_ip 6379" >> /etc/redis.conf
 echo "masterauth $redis_master_password" >> /etc/redis.conf
-systemctl restart redis
+
+systemctl stop redis
+systemctl start redis
